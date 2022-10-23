74402a7fa6a9989d1dafc6f81e04e9b1a7a307f6
scorpi000 <scorpi000.com>
2022-10-23 17:32:44 +0800

    Docker

--- QSExt/Docker/FactorDef/start.sh
+++ QSExt/Docker/FactorDef/start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 cd ./FactorDef
-airflow db init
-airflow users create --username admin --firstname Peter --lastname Parker --role Admin --password 123.com --email scorpi000@163.com
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    airflow db init
+    airflow users create --username admin --firstname Peter --lastname Parker --role Admin --password 123.com --email scorpi000@163.com
+else
+    echo "-- Not first container startup --"
+fi
+
 nohup airflow scheduler --daemon --subdir /usr/src/app/DAGs > airflow_scheduler.log 2>&1 &
 airflow webserver
