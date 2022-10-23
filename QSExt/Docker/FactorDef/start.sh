5ffd9be3e83b75daee4c8906ae96cd9700002883
scorpi000 <scorpi000@163.com>
2022-10-23 23:50:56 +0800

    docker

--- QSExt/Docker/FactorDef/start.sh
+++ QSExt/Docker/FactorDef/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-cd ./FactorDef
+#cd ./FactorDef
 
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+if [ ! -e $CONTAINER_ALREADY_STARTED ]
+then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
     airflow db init
