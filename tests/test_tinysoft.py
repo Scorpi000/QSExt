8bd313106a5dfec5f5f1c7971ef054797607464e
Scorpi000 <scorpi000@sina.cn>
2026-06-22 10:21:44 +0800

    tinysoft

--- tests/test_tinysoft.py
+++ tests/test_tinysoft.py
@@ -15,29 +15,40 @@ with open(__QS_ConfigPath__ + os.sep + "TinySoftDBConfig.json", mode="r") as fp:
 # 测试连接
 c = pyTSL.Client(conn_args["User"], conn_args["Pwd"], conn_args["IPAddr"], conn_args["Port"])
 c.login()
-r = c.exec('''return "测试"; ''')
-print(r.value())
 
-# 读取数据
-code='''
-function get_data(begT, endT);
-begin
-    return select * from markettable datekey begT to endT of DefaultStockID() end;
-end;
-'''
-r = c.call('get_data', dt.datetime(2026, 6, 15), dt.datetime.now(), stock="SZ000001", cycle="日线", code=code)
-# r.value() 方法可以指定需要转换成datetime类型的字段
+# r = c.exec('''return "测试"; ''')
+# print(r.value())
+
+# # 读取数据
+# code='''
+# function get_data(begT, endT);
+# begin
+#     return select * from markettable datekey begT to endT of DefaultStockID() end;
+# end;
+# '''
+# r = c.call('get_data', dt.datetime(2026, 6, 15), dt.datetime.now(), stock="SZ000001", cycle="日线", code=code)
+# # r.value() 方法可以指定需要转换成datetime类型的字段
+# df = pd.DataFrame(data=r.value(parse_date=['date']))
+# df = df.set_index(["date"])
+# print(df.head())
+
+# # pyTSL也提供了把返回结果直接转成DataFrame的方法
+# df = r.dataframe()
+# # pyTSL提供了TSL的时间类型和python的datetime类型的转换方法
+# # DatetimeToDouble 转换datetime类型到TSL的时间类型
+# # DoubleToDatetime 转换TSL的时间类型到datetime类型
+# df['date'] = df['date'].apply(lambda x: pyTSL.DoubleToDatetime(x))
+# df = df.set_index(["date"])
+# print(df.head())
+
+# markettable
+code = """SetSysParam(pn_cycle(), cy_trailingseconds(60));
+return select * from markettable datekey inttodate(20260617) to (inttodate(20260617) + 0.9999) of 'IF2607' end;
+"""
+r = c.exec(code)
 df = pd.DataFrame(data=r.value(parse_date=['date']))
 df = df.set_index(["date"])
-print(df.head())
-
-# pyTSL也提供了把返回结果直接转成DataFrame的方法
-df = r.dataframe()
-# pyTSL提供了TSL的时间类型和python的datetime类型的转换方法
-# DatetimeToDouble 转换datetime类型到TSL的时间类型
-# DoubleToDatetime 转换TSL的时间类型到datetime类型
-df['date'] = df['date'].apply(lambda x: pyTSL.DoubleToDatetime(x))
-df = df.set_index(["date"])
+print(df.shape)
 print(df.head())
 
 c.logout()
